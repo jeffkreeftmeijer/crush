ab287f86b60185b940a0b7090cbd288c0a87c005
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2021-07-22 19:18:54 +0200

    Minify with cssnano

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-exports.crush = function (input) {
-  return input;
+const postcss = require("postcss");
+const cssnano = require("cssnano");
+const regex = /<style>(.*?)<\/style>/gs;
+
+exports.crush = async function (input) {
+  let promises = [...input.matchAll(regex)].map(process);
+  let replacements = await Promise.all(promises);
+
+  return input.replace(regex, () => replacements.shift());
 };
+
+function process(match) {
+  return postcss(cssnano())
+    .process(match[1])
+    .then((result) => {
+      return match[0].replace(match[1], result.css);
+    });
+}
