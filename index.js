6efd646e8055879ce78d1e8255b2f65e34bb4ffc
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2021-07-22 20:10:51 +0200

    Purge css with purgecss

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const postcss = require("postcss");
 const cssnano = require("cssnano");
+const purgecss = require("@fullhuman/postcss-purgecss");
 const regex = /<style>(.*?)<\/style>/gs;
 
 exports.crush = async function (input) {
-  let promises = [...input.matchAll(regex)].map(process);
+  let promises = [...input.matchAll(regex)].map((match) => {
+    return process(match, input);
+  });
   let replacements = await Promise.all(promises);
 
   return input.replace(regex, () => replacements.shift());
 };
 
-function process(match) {
-  return postcss(cssnano())
+function process(match, html) {
+  return postcss([
+    purgecss({ content: [{ raw: html.replace(match[1], "") }] }),
+    cssnano(),
+  ])
     .process(match[1])
     .then((result) => {
       return match[0].replace(match[1], result.css);
